58231f065da910fed6c6a10d6a3fe3edbeb0543b
Max Nardit <mnardit@gmail.com>
2025-01-30 09:32:03 +0700

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,16 @@
+async function middleware(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://zimm.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Max-Age', '86400');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  await next();
+}
+
+export default middleware;
